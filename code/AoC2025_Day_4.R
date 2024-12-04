8533f8d33f4186456b16c4709de76f522374ff7a
Olivier Pucher <olivier.pucher@insee.fr>
2024-12-04 10:19:36 +0100

    Day 4

--- code/AoC2025_Day_4.R
+++ code/AoC2025_Day_4.R
@@ -3,14 +3,64 @@
 source("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/code/packages.R")
 source("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/code/usefulFunctions.R")
 
-dat <- readLines("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/data/D4.txt")
-dat <- read.csv2("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/data/D4.txt")
+dat <- read.csv2("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/data/D4.txt",header = FALSE)
+m <- df_from_vector_of_strings(dat$V1) %>% as.matrix
 
 # A
-
-
-
+res <- 0
+for(j in 1:(ncol(m)-3)){
+  for(i in 1:nrow(m)){
+    if(m[i,j]=="X" & m[i,j+1]=="M" & m[i,j+2]=="A" & m[i,j+3]=="S") res <- res+1
+  }
+}
+for(j in 1:ncol(m)){
+  for(i in 1:(nrow(m)-3)){
+    if(m[i,j]=="X" & m[i+1,j]=="M" & m[i+2,j]=="A" & m[i+3,j]=="S") res <- res+1
+  }
+}
+for(j in 4:ncol(m)){
+  for(i in 1:nrow(m)){
+    if(m[i,j]=="X" & m[i,j-1]=="M" & m[i,j-2]=="A" & m[i,j-3]=="S") res <- res+1
+  }
+}
+for(j in 1:ncol(m)){
+  for(i in 4:nrow(m)){
+    if(m[i,j]=="X" & m[i-1,j]=="M" & m[i-2,j]=="A" & m[i-3,j]=="S") res <- res+1
+  }
+}
+for(j in 1:(ncol(m)-3)){
+  for(i in 1:(nrow(m)-3)){
+    if(m[i,j]=="X" & m[i+1,j+1]=="M" & m[i+2,j+2]=="A" & m[i+3,j+3]=="S") res <- res+1
+  }
+}
+for(j in 1:(ncol(m)-3)){
+  for(i in 4:nrow(m)){
+    if(m[i,j]=="X" & m[i-1,j+1]=="M" & m[i-2,j+2]=="A" & m[i-3,j+3]=="S") res <- res+1
+  }
+}
+for(j in 4:ncol(m)){
+  for(i in 4:nrow(m)){
+    if(m[i,j]=="X" & m[i-1,j-1]=="M" & m[i-2,j-2]=="A" & m[i-3,j-3]=="S") res <- res+1
+  }
+}
+for(j in 4:ncol(m)){
+  for(i in 1:(nrow(m)-3)){
+    if(m[i,j]=="X" & m[i+1,j-1]=="M" & m[i+2,j-2]=="A" & m[i+3,j-3]=="S") res <- res+1
+  }
+}
+res
 
 # B
-
-
+res <- 0
+for(j in 2:(ncol(m)-1)){
+  for(i in 2:(nrow(m)-1)){
+    if(m[i,j]=="A"){
+      if((m[i-1,j-1]=="M" & m[i+1,j+1]=="S") || (m[i-1,j-1]=="S" & m[i+1,j+1]=="M")){
+        if((m[i-1,j+1]=="M" & m[i+1,j-1]=="S") || (m[i-1,j+1]=="S" & m[i+1,j-1]=="M")){
+          res <- res+1
+        }
+      } 
+    }
+  }
+}
+res
---
+++ code/AoC2025_Day_5.R
@@ -0,0 +1,13 @@
+# Aoc 2024 Day 5
+
+source("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/code/packages.R")
+source("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/code/usefulFunctions.R")
+
+dat <- readLines("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/data/D5.txt")
+dat <- read.csv2("C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2024/advent_of_code_2024/data/D5.txt",sep = "",header = FALSE)
+
+# A
+
+
+# B
+
