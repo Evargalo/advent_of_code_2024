88273d299c49d98bc320e5bf5faba5a76bf926a4
Olivier Pucher <olivier.pucher@insee.fr>
2024-12-06 17:25:15 +0100

    Update AoC2025_Day_6.R

--- code/AoC2025_Day_6.R
+++ code/AoC2025_Day_6.R
@@ -89,63 +89,6 @@ history %<>%
 
 mm <- maze$Maze 
 
-eval_change <- function(xx,yy){
-  
-  m <- mm
-  if(m %>% filter(x==xx,y==yy) %>% pull(t) != ".") return(FALSE)
-  m %<>% mutate(t=if_else(x==xx & y==yy,"#",t))
-  currentpos <- m %>% filter(t=="^")
-  dir <- dirs[1]
-  out <- FALSE
-  xx <- currentpos$x
-  yy <- currentpos$y
-  loop <- FALSE
-  short_history <- tibble(x=integer(),y=integer(),t=character(),dir=integer())
-  
-  while(!out & !loop){
-    m %<>% mutate(t=if_else(x==xx & y==yy,"X",t))
-    ddir <- dir
-    if(nrow(short_history %>% filter(x==xx,y==yy,dir==ddir)) >0 ){
-      loop <- TRUE
-      next()
-    }
-    short_history %<>% add_row(x=xx,y=yy,t="X",dir=ddir)
-    xx <- xx + xDir[dir]
-    yy <- yy + yDir[dir]
-    if(xx==0 | xx>xMax | yy==0 | yy>yMax){
-      out <- TRUE
-      next()
-    }
-    tt <- m %>% filter(x==xx,y==yy) %>% pull(t) %>% unlist
-    if(tt=="#"){
-      xx <- xx - xDir[dir]
-      yy <- yy - yDir[dir]
-      dir <- (dir %% 4) + 1
-    }
-  }
-  return(loop)
-}
-
-eval_change(6,5)
-
-res <- tibble(x=integer(),y=integer())
-res %<>% mutate(change=TRUE) 
-for(i in 1:(nrow(history)-1)){
-  # for(i in 186:(nrow(history)-1)){
-  if(i%%10==0) print(i)
-  ll <- history %>% filter(rank==i)
-  xx <- ll$x + xDir[ll$dir]
-  yy <- ll$y + yDir[ll$dir]
-  if(nrow(res %>% filter(x==xx,y==yy))>0) next()
-  if(eval_change(xx,yy)) {
-    res %<>% add_row(x=xx,y=yy,change=TRUE)
-  } else{
-    res %<>% add_row(x=xx,y=yy,change=FALSE)
-  }
-}
-res %>% group_by(change) %>% count 
-
-# 4971 too high
 
 m<-mm %>% filter(t=="#")
 
@@ -158,7 +101,7 @@ next_step <- function(.x,.y,.dir,m=mm){
   return(res %>% mutate(dir=(.dir%%4)+1))
 }
 
-# A
+# Pour refaire le A
 cur <- currentpos %>% select(-t) %>% mutate(l=0)
 road <- cur
 while(nrow(cur)>0){
@@ -199,7 +142,6 @@ eval_change <- function(xx,yy){
 res <- tibble(x=integer(),y=integer())
 res %<>% mutate(change=TRUE) 
 for(i in 1:(nrow(history)-1)){
- # for(i in 1590:(nrow(history)-1)){
   if(i%%10==0) print(i)
   ll <- history %>% filter(rank==i)
   xx <- ll$x + xDir[ll$dir]
@@ -213,4 +155,4 @@ for(i in 1:(nrow(history)-1)){
 }
 res %>% group_by(change) %>% count 
 
-
+# 1482
